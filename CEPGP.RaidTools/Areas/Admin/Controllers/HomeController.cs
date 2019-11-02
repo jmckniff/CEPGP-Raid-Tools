ccaf81ab35a28e055a2eefed3190ab48564ac39e
Jamie McKniff <Jamie.McKniff@everyday-loans.co.uk>
2019-11-02 17:26:30 +0000

    Removed the CEPGP prefix from page titles as its superfluous

---
+++ CEPGP.RaidTools/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CEPGP.RaidTools.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
